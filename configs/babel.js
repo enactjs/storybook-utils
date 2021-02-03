75b5c6513bcb7df04ceb637914db3dc55e17b82f
Jeongran Jang <jeongran.jang@lge.com>
2021-02-03 09:06:49 +0900

    Remove new JSX transform plugin for React17
    Enact-DCO-1.0-Signed-off-by: Jeongran Jang (jeongran.jang@lge.com)

--- configs/babel.js
+++ configs/babel.js
@@ -81,7 +81,6 @@ module.exports = function (api) {
 			require('@babel/plugin-proposal-nullish-coalescing-operator').default,
 
 			require('babel-plugin-dev-expression'),
-			require('@babel/plugin-transform-react-jsx'),
 			env === 'test' && !es5Standalone && require('babel-plugin-dynamic-import-node').default,
 			env === 'production' && !es5Standalone && require('@babel/plugin-transform-react-inline-elements').default
 		].filter(Boolean)
