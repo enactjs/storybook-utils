f3d5d98c68d5c42c514fbfb2998cb59ee0d1a013
Mikyung Kim <mikyung27.kim@lge.com>
2021-02-16 17:56:32 +0900

    Removed unused code
    Enact-DCO-1.0-Signed-off-by: Mikyung Kim (mikyung27.kim@lge.com)

--- configs/babel.js
+++ configs/babel.js
@@ -7,10 +7,6 @@
 
 // Check if JSX transform is able
 const hasJsxRuntime = (() => {
-	if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
-		return false;
-	}
-
 	try {
 		require.resolve('react/jsx-runtime');
 		return true;
