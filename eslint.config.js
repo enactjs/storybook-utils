098dad3cfb2d8508dd98f0d915bfd0f71e9d3191
Stanca Pop <stanca.pop@lgepartner.com>
2023-02-20 17:09:36 +0200

    test

--- eslint.config.js
+++ eslint.config.js
@@ -20,13 +20,13 @@ module.exports = [
 		},
 		rules: {
 			'react/forbid-foreign-prop-types': 'off', // proptypes not removed in storybook config
-			'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
-			'import/named': 'error',
-			'import/first': 'warn',
-			'import/no-duplicates': 'error',
-			'import/extensions': ['warn', 'always', {js: 'never', json: 'always'}],
-			'import/newline-after-import': 'warn',
-			'import/order': [
+			'importPlugin/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
+			'importPlugin/named': 'error',
+			'importPlugin/first': 'warn',
+			'importPlugin/no-duplicates': 'error',
+			'importPlugin/extensions': ['warn', 'always', {js: 'never', json: 'always'}],
+			'importPlugin/newline-after-import': 'warn',
+			'importPlugin/order': [
 				'warn',
 				{
 					'newlines-between': 'never',
