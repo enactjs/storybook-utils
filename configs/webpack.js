268e8f24ce313a2be2a939dbd0fa115c54539161
taeyoung.hong <taeyoung.hong@lge.com>
2024-06-20 19:26:20 +0900

    add css-loader config for applying 6.x behavior 2

--- configs/webpack.js
+++ configs/webpack.js
@@ -29,7 +29,8 @@ module.exports = function (config, mode, dirname) {
 						// Options to restore 6.x behavior:
 						// https://github.com/webpack-contrib/css-loader/blob/master/CHANGELOG.md#700-2024-04-04
 						modules: {
-							namedExport: false
+							namedExport: false,
+							exportLocalsConvention: 'as-is'
 						}
 					},
 					cssLoaderOptions
