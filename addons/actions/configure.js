30e706abd3a95c1811f2a66482d2d16f3af7e208
dongsu.won <dongsu.won@lgepartner.com>
2024-07-26 10:29:57 +0900

    * Increase limit for the number of items logged into the actions panel
     Enact-DCO-1.0-Signed-off-by: Dongsu Won (dongsu.won@lgepartner.com)

--- addons/actions/configure.js
+++ addons/actions/configure.js
@@ -2,6 +2,9 @@ import {configureActions as addonConfigureActions} from '@storybook/addon-action
 
 const configureActions = opts => {
 	return addonConfigureActions({
+		// TODO: Set to limit of 200 is a workaround. We need to update event display to sort in latest order.
+		// Limit the number of items logged into the actions panel
+		limit: 200,
 		...opts
 	});
 };
