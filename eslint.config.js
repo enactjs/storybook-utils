920587c839c048fb879d236c70cb06beb45e0b29
Stanca <stanca.pop@lgepartner.com>
2025-01-23 17:10:04 +0200

    updated to eslint 9 and adopted flat config

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+const enactConfig = require('eslint-config-enact');
+const prettierConfig = require('eslint-config-prettier');
+const importPlugin = require('eslint-plugin-import');
+const prettierPlugin = require('eslint-plugin-prettier');
+const globals = require('globals');
+
+module.exports = [
+	...enactConfig,
+	{
+		files: ['*/.js', '*/.jsx', '*/.ts', '*/.tsx'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.node
+			},
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true
+				}
+			}
+		},
+		plugins: {
+			import: importPlugin,
+			prettier: prettierPlugin
+		}
+	},
+	{
+		plugins: {
+			import: importPlugin
+		},
+		rules: {
+			'react/forbid-foreign-prop-types': 'off', // proptypes not removed in storybook config
+			'import/no-unresolved': ['error', {commonjs: true, caseSensitive: true}],
+			'import/named': 'error',
+			'import/first': 'warn',
+			'import/no-duplicates': 'error',
+			'import/extensions': ['warn', 'always', {js: 'never', json: 'always'}],
+			'import/newline-after-import': 'warn',
+			'import/order': [
+				'warn',
+				{
+					'newlines-between': 'never',
+					groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index']
+				}
+			],
+			'@stylistic/js/space-before-function-paren': [
+				'warn',
+				{
+					named: 'never'
+				}
+			]
+		}
+	},
+	{
+		plugins: {
+			prettier: prettierPlugin
+		},
+		rules: {
+			...prettierPlugin.configs.recommended.rules,
+			...prettierConfig.rules
+		}
+	}
+];
